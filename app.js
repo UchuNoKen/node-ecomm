bd7395bc7fe8122cda5d9591a4a5e35c1540b9ed
UchuNoKen <xen.o.fite@gmail.com>
2019-05-28 21:26:14 -0400

    add admin and shop routes

--- app.js
+++ app.js
@@ -4,22 +4,15 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form>'
-  );
-});
+app.use(adminRoutes);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Express Routing</h1>");
-});
+app.use(shopRoutes);
 
 app.listen(8000);
+
 console.log("Server running on port: 8000");
