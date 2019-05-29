063079a09af8dc3c732b7d3a27b404d941a48c42
UchuNoKen <xen.o.fite@gmail.com>
2019-05-28 21:33:11 -0400

    add 404 page

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.use(adminRoutes);
 
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
 app.listen(8000);
 
 console.log("Server running on port: 8000");
