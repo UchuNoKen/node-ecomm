04f17441be4c09806290c5f12089e477c1ae0ffb
UchuNoKen <xen.o.fite@gmail.com>
2019-05-30 18:09:05 -0400

    add error controller

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const errorController = require("./controllers/error");
 const app = express();
 
 // global configuration value
@@ -19,9 +20,7 @@ app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/" });
-});
+app.use(errorController.get404);
 
 app.listen(8000);
 
