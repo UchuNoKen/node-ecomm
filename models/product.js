fd2daaed4f0a46ad3334750dbb56309fa90426d5
UchuNoKen <xen.o.fite@gmail.com>
2019-06-01 13:48:19 -0400

    save and fetch data

--- models/product.js
+++ models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+// const products = [];
 
 module.exports = class Product {
   constructor(t) {
@@ -6,10 +9,30 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(path.dirname(process.mainModule.filename), "data", "products.json");
+    // products.push(this);
+
+    fs.readFile(p, (err, fileContent) => {
+      // console.log(fileContent);
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    const p = path.join(path.dirname(process.mainModule.filename), "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
   }
 };
